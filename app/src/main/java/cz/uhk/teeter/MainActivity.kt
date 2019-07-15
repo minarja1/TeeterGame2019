11cbcf9379ef024330b8168297fa350446ca5239
Jakub Minarik <minarik@mobilesoft.eu>
2019-07-15 10:00:39 +0200

    Fullscreen and runnable

--- app/src/main/java/cz/uhk/teeter/MainActivity.kt
+++ app/src/main/java/cz/uhk/teeter/MainActivity.kt
@@ -2,16 +2,43 @@ package cz.uhk.teeter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val FPS = 120L
+    private lateinit var handler: SensorHandler
+    private var init = false
+    private lateinit var level: Level
+
+    private val handlerOs = Handler()
+    private val runnable: Runnable = object : Runnable {
+        override fun run() {
+            if (init) {
+                draw()
+                detectFails()
+                detectWin()
+            }
+            handlerOs.postDelayed(this, 1000 / FPS)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        window?.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        window?.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         setContentView(R.layout.activity_main)
 
-        // todo
-        // fullscreen
+
         // setting of surface view
         // sensor handler initialization
     }
@@ -35,7 +62,7 @@ class MainActivity : AppCompatActivity() {
         surfaceView.post {
             // later level init
 
-            SensorHandler().init(surfaceView, Level(), Ball())
+
         }
         // loading of level in try catch
     }
