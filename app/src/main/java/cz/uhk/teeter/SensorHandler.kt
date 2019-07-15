943fa5002486f4da58757be1af7a40005a6b5c3b
Jakub Minarik <minarik@mobilesoft.eu>
2019-07-15 11:23:50 +0200

    Velocity computing

--- app/src/main/java/cz/uhk/teeter/SensorHandler.kt
+++ app/src/main/java/cz/uhk/teeter/SensorHandler.kt
@@ -9,23 +9,61 @@ import android.view.SurfaceView
 
 class SensorHandler : SensorEventListener {
 
-    lateinit var ball:Ball
+    lateinit var ball: Ball
+
+    val FRICTION = 0.95f
+    val REFLECTION = 0.99f
+
+    val GRAVITY = 9.8f
+    val NOISE = 0.35f
+
+    val ALPHA = 0.8f
+    val gravity = floatArrayOf(0f, 0f, 0f)
+
+    var lastMillis = 0L
 
     override fun onSensorChanged(event: SensorEvent?) {
 
-        // calculation of velocity, acceleration and ball position
+        val nowMillis = System.currentTimeMillis()
 
-        val x = event?.values?.get(0)
-        val y = event?.values?.get(1)
-        val z = event?.values?.get(2)
+        event?.values?.run {
 
-        val format = "%.4f"
+            // this is how to get rid of the noise
+            val clearValues = floatArrayOf(
+                clear(this[0]),
+                clear(this[1]),
+                clear(this[2])
+            )
 
-        println("X: " + format.format(x) +
-                ", Y: " + format.format(y) +
-                ", Z: " + format.format(z))
+            // low pass filter
+            // we will use 80% of the old values
+            gravity[0] = ALPHA * gravity[0] + (1 - ALPHA) * clearValues[0]
+            gravity[1] = ALPHA * gravity[1] + (1 - ALPHA) * clearValues[1]
+            gravity[2] = ALPHA * gravity[2] + (1 - ALPHA) * clearValues[2]
 
-        // X: 0.3456, Y: 3.3456, Z: 6.5433
+            // normalization (wtf?)
+            // gravity[0] = -1.2, gravity[1] = 0.8, gravity[2] = 7.3
+            // gravity[0] + gravity[1] + gravity[2] = 1.
+
+            val sum = Math.abs(gravity[0])
+            +Math.abs(gravity[1])
+            +Math.abs(gravity[2])
+
+            // changing values to have 9.8 in total x+y+z
+            gravity[0] = (gravity[0] / sum * GRAVITY)
+            gravity[1] = (gravity[1] / sum * GRAVITY)
+            gravity[2] = (gravity[2] / sum * GRAVITY)
+
+            // getting delta of passed time
+            val deltaTime = (nowMillis - lastMillis).toFloat() / 1000F
+
+            // getting the new velocity of ball from values
+            // v = a*t    (v = velocity, a = acceleration, t = time)
+            val newVelocityX = gravity[0] * deltaTime
+            val newVelocityY = gravity[1] * deltaTime
+
+
+        }
 
     }
 
@@ -38,15 +76,20 @@ class SensorHandler : SensorEventListener {
         val manager = surfaceView.context
             .getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-        manager.registerListener(this,
+        manager.registerListener(
+            this,
             manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_FASTEST)
+            SensorManager.SENSOR_DELAY_FASTEST
+        )
 
     }
 
-    private fun clear() {
-
+    private fun clear(value: Float): Float {
         // removing of noise from low pass filter
+        return when {
+            value < NOISE && value > -NOISE -> 0F
+            else -> value
+        }
     }
 
 
