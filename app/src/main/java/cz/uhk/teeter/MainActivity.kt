b72ff40848351f9c9dd2971032d6790f94689f01
Jakub Minarik <minarik@mobilesoft.eu>
2019-07-15 09:39:16 +0200

    Println of accelerometer values

--- app/src/main/java/cz/uhk/teeter/MainActivity.kt
+++ app/src/main/java/cz/uhk/teeter/MainActivity.kt
@@ -2,6 +2,7 @@ package cz.uhk.teeter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +32,11 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        surfaceView.post {
+            // later level init
+
+            SensorHandler().init(surfaceView, Level(), Ball())
+        }
         // loading of level in try catch
     }
 
--- app/src/main/java/cz/uhk/teeter/SensorHandler.kt
+++ app/src/main/java/cz/uhk/teeter/SensorHandler.kt
@@ -4,18 +4,40 @@ import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.view.SurfaceView
 
 class SensorHandler : SensorEventListener {
 
-    override fun onSensorChanged(p0: SensorEvent?) {
+    override fun onSensorChanged(event: SensorEvent?) {
 
         // calculation of velocity, acceleration and ball position
+
+        val x = event?.values?.get(0)
+        val y = event?.values?.get(1)
+        val z = event?.values?.get(2)
+
+        val format = "%.4f"
+
+        println("X: " + format.format(x) +
+                ", Y: " + format.format(y) +
+                ", Z: " + format.format(z))
+
+        // X: 0.3456, Y: 3.3456, Z: 6.5433
+
     }
 
     fun init(surfaceView: SurfaceView, level: Level, ball: Ball) {
 
         // initialization of a game
+
+        val manager = surfaceView.context
+            .getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        manager.registerListener(this,
+            manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_FASTEST)
+
     }
 
     private fun clear() {
