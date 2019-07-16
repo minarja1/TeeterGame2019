0bb8fc89b48c10fedb042278df30ed34e1ca9d6c
Jakub Minarik <minarik@mobilesoft.eu>
2019-07-16 10:07:04 +0200

    OBstacle left collison

--- app/src/main/java/cz/uhk/teeter/Obstacle.kt
+++ app/src/main/java/cz/uhk/teeter/Obstacle.kt
@@ -7,9 +7,29 @@ class Obstacle {
     var x2 = 0
     var y2 = 0
 
+    val REFLECTION = 0.99f
+
     fun handleCollisions(ball: Ball) {
 
         // handling of collisions with ball
-        // TODO handling collisions
+        val ballPosition = Point2D().apply {
+            x = ball.position.x.metersToPx
+            y = ball.position.y.metersToPx
+        }
+
+        // left
+        if (ballPosition.x > x - ball.radius
+            && ballPosition.x < x + ball.radius
+            && ballPosition.y > y - ball.radius
+            && ballPosition.y < y2 + ball.radius
+        ) {
+            ball.position.x = (x2 + ball.radius).toFloat().pxToMeters
+            ball.velocityX = ball.velocityX * (-REFLECTION)
+        }
+
+        // todo right
+        // todo top
+        // todo bottom
+
     }
 }
